f3ecd49df191b76f3b29f4140614efe92bf545f9
s28539 <s28539@pjwstk.edu.pl>
2024-06-11 14:33:16 +0200

    second commit

--- WebApplication1/Data/ApplicationContext.cs
+++ WebApplication1/Data/ApplicationContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Models;
 
 namespace WebApplication1;
 
@@ -10,5 +11,59 @@ public class ApplicationContext : DbContext
 
     public ApplicationContext(DbContextOptions options) : base(options)
     {
+    }
+    public DbSet<backpacks> Backpacks { get; set; }
+    public DbSet<characters> Characters { get; set; }
+    public DbSet<characters_titles> CharactersTitles { get; set; }
+    public DbSet<items> Items { get; set; }
+    public DbSet<titles> Titles { get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<items>().HasData(new List<items>
+        {
+            new items {
+                Id = 1,
+                Name = "Bidon",
+                Weight = 2
+            }
+        });
+        modelBuilder.Entity<backpacks>().HasData(new List<backpacks>
+        {
+            new backpacks {
+                CharacterId = 1,
+                ItemId = 1,
+                Amount = 2
+            }
+        });
+        modelBuilder.Entity<characters>().HasData(new List<characters>
+        {
+            new characters {
+                Id = 1,
+                FirstName = "Mariusz",
+                LastName = "Kowalski",
+                CurrentWeight = 20,
+                MaxWeight = 30
+            }
+        });
+        
+        modelBuilder.Entity<characters_titles>().HasData(new List<characters_titles>
+        {
+            new characters_titles {
+                CharacterId = 1,
+                TitleId = 1,
+                AcquriedAt = DateTime.Parse("2024-05-31")
+            }
+        });
+        
+        modelBuilder.Entity<titles>().HasData(new List<titles>
+        {
+            new titles {
+                Id = 1,
+                Name = "example1"
+
+            }
+        });
+        
     }
 }
